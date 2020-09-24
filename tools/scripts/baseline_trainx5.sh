c69ed6fa183b09b1d12538a2ae3e8f9ce08a903c
rasd3 <yckim@spa.hanyang.ac.kr>
2020-09-24 17:32:10 +0900

    add trin.sh

---
+++ tools/scripts/baseline_trainx5.sh
@@ -0,0 +1,3 @@
+bash scripts/dist_train.sh 2 --cfg_file=./cfgs/kitti_models/pv_rcnn.yaml --extra_tag=baseline_8020_4
+bash scripts/dist_train.sh 2 --cfg_file=./cfgs/kitti_models/pv_rcnn.yaml --extra_tag=baseline_8020_5
+bash scripts/dist_train.sh 2 --cfg_file=./cfgs/kitti_models/pv_rcnn.yaml --extra_tag=baseline_8020_6
